d4886df26fa14d83c3126469532c276225dde2eb
Harisha M R <harishamr@example.com>
2025-01-29 09:59:36 -0500

    Implementaion rating done

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -5,8 +5,10 @@ const FeedbackForm = () => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
-        feedback: ''
+        feedback: '',
+        rating: ''
     });
+
 
     const handleChange = (event) => {
         const { name, value } = event.target;
@@ -18,7 +20,8 @@ const FeedbackForm = () => {
         const confirmationMessage = `
         name: ${formData.name}
         Email: ${formData.email}
-        Feedback: ${formData.feedback}`;
+        Feedback: ${formData.feedback}
+        rating:${formData.rating}`;
 
         const isConfirmed = window.confirm(`Please confirm your details:\n\n${confirmationMessage}`);
         if (isConfirmed) {
@@ -26,7 +29,8 @@ const FeedbackForm = () => {
             setFormData({
                 name: '',
                 email: '',
-                feedback: ''
+                feedback: '',
+                rating: ''
             });
             alert('Thank you for your valuable feedback!');
         }
@@ -39,10 +43,18 @@ const FeedbackForm = () => {
             <form className="feedback-form" onSubmit={handleSubmit}>
                 <h2>We'd Love to Hear From You!</h2>
                 <p>Please share your feedback with us.</p>
-                <input type="text" name="name" placeholder="Your name"  value={formData.name} onChange={handleChange}/>
-                <input type="email" name="email" placeholder='Your email' value = {formData.email} onChange={handleChange}/>
-                <textarea name="feedback" placeholder='Your feedback' value = {formData.feedback} onChange={handleChange}/>
-                <button type='submit'>Submit Feedback</button>
+                <input type="text" name="name" placeholder="Your name" value={formData.name} onChange={handleChange} />
+                <input type="email" name="email" placeholder='Your email' value={formData.email} onChange={handleChange} />
+                <textarea name="feedback" placeholder='Your feedback' value={formData.feedback} onChange={handleChange} />
+                <div style={{ display: 'flex', gap: '10px', flexDirection: 'column' }}>
+                    <span>Rate Us:</span>
+                    <p><input type="radio" name="rating" value="1" onChange={handleChange} />1</p>
+                    <p><input type="radio" name="rating" value="2" onChange={handleChange} />2</p>
+                    <p><input type="radio" name="rating" value="3" onChange={handleChange} />3</p>
+                    <p><input type="radio" name="rating" value="4" onChange={handleChange} />4</p>
+                    <p><input type="radio" name="rating" value="5" onChange={handleChange} />5</p>
+                    <p></p><button type='submit'>Submit Feedback</button>
+                </div>
             </form>
         </>
     );
